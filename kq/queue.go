182a182f5c42c693b234b95df10a5c82482bcdb4
chengfield <291179247@qq.com>
2025-01-09 16:23:03 +0800

    change init consumer func

--- kq/queue.go
+++ kq/queue.go
@@ -34,12 +34,12 @@ const (
 )
 
 type (
-	ConsumeHandle func(ctx context.Context, key, value string) error
+	ConsumeHandle func(ctx context.Context, key kafka.Message) error
 
 	ConsumeErrorHandler func(ctx context.Context, msg kafka.Message, err error)
 
 	ConsumeHandler interface {
-		Consume(ctx context.Context, key, value string) error
+		Consume(ctx context.Context, key kafka.Message) error
 	}
 
 	kafkaReader interface {
@@ -171,7 +171,7 @@ func newKafkaQueue(c KqConf, handler ConsumeHandler, options queueOptions) queue
 	}
 	if c.CommitInOrder {
 		q.commitRunner = threading.NewStableRunner(func(msg kafka.Message) kafka.Message {
-			if err := q.consumeOne(context.Background(), string(msg.Key), string(msg.Value)); err != nil {
+			if err := q.consumeOne(context.Background(), msg); err != nil {
 				if q.errorHandler != nil {
 					q.errorHandler(context.Background(), msg, err)
 				}
@@ -211,9 +211,9 @@ func (q *kafkaQueue) Stop() {
 	logx.Close()
 }
 
-func (q *kafkaQueue) consumeOne(ctx context.Context, key, val string) error {
+func (q *kafkaQueue) consumeOne(ctx context.Context, key kafka.Message) error {
 	startTime := timex.Now()
-	err := q.handler.Consume(ctx, key, val)
+	err := q.handler.Consume(ctx, key)
 	q.metrics.Add(stat.Task{
 		Duration: timex.Since(startTime),
 	})
@@ -231,7 +231,7 @@ func (q *kafkaQueue) startConsumers() {
 				// remove deadline and error control
 				ctx = contextx.ValueOnlyFrom(ctx)
 
-				if err := q.consumeOne(ctx, string(msg.Key), string(msg.Value)); err != nil {
+				if err := q.consumeOne(ctx, msg); err != nil {
 					if q.errorHandler != nil {
 						q.errorHandler(ctx, msg, err)
 					}
@@ -346,8 +346,8 @@ type innerConsumeHandler struct {
 	handle ConsumeHandle
 }
 
-func (ch innerConsumeHandler) Consume(ctx context.Context, k, v string) error {
-	return ch.handle(ctx, k, v)
+func (ch innerConsumeHandler) Consume(ctx context.Context, k kafka.Message) error {
+	return ch.handle(ctx, k)
 }
 
 func ensureQueueOptions(c KqConf, options *queueOptions) {
